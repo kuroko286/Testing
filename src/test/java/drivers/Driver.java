f31e65245bb4e707d2d9ee9d9cd1016effd9de93
Lê Văn Quốc <94890892+kuroko286@users.noreply.github.com>
2023-09-19 21:51:08 +0700

    first

---
+++ src/test/java/drivers/Driver.java
@@ -0,0 +1,22 @@
+package drivers;
+
+public class Driver {
+    public String checkHealthy(double bmi) {
+        if (bmi <= 0) {
+            return "Invalid";
+        } else if (bmi < 18.5) {
+            return "Underweight";
+        } else if (bmi < 25) {
+            return "Normal";
+        } else if (bmi < 30) {
+            return "Overweight";
+        } else {
+            return "Obesity";
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+
+    }
+}
